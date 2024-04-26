280b1b104d50915be2802d3ecb37024f164b50a2
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-04-26 14:58:53 +0500

    first

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquier from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.bold.blueBright('\n \t \t Well come to word counter'))
+console.log("=".repeat(70))
+
+
+let answer = await inquier.prompt({
+    name:"sentence",
+    type:'input',
+    message:"Enter your sentence"
+})
+
+let word = answer.sentence.trim().split(" ");
+
+console.log("=".repeat(70))
+console.log(chalk.bold.green("\t \t Sentence word :"))
+console.log(word)
+console.log("=".repeat(70))
+console.log(`${chalk.bold.redBright("\t \t Sentence count :")}${word.length}`)
